49002666401ee47d1831e396c066227c40fba404
Deepak Kumar <dk135781@gmail.com>
2023-12-23 09:28:43 +0530

    3rd commit

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,8 @@ const express=require('express');
 const router = express.Router();
 
 
-router.use('/',require('./user'));
 
-// router.use('/user', require('./users'));
-// router.use('/auth', require('./auth'));
+router.use('/user', require('./users'));
+router.use('/auth', require('./auth'));
 
 module.exports=router;
