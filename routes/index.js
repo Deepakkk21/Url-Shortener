af54fe717001e75a91304a092844d3996b341463
Deepak <113232000+Deepakkk21@users.noreply.github.com>
2023-12-23 15:48:56 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const router = express.Router();
 
+const userController = require('../controllers/users_controller')
 
+router.get('/' , userController.Homepage) 
 router.use('/user', require('./users'));
 router.use('/auth', require('./auth'));
 
