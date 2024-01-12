c2915ee9276b25d40b700d4bf9f022bcd985cfea
Deepak Kumar <dk135781@gmail.com>
2024-01-12 19:32:49 +0530

    db updated

--- modals/URL.js
+++ modals/URL.js
@@ -2,9 +2,21 @@
 
 const { Sequelize, DataTypes } = require('sequelize');
 
-const sequelize = new Sequelize('Url_shortener', 'postgres', 'Deep@k2110', {
-  host: 'localhost',
-  dialect: 'postgres',
+// const sequelize = new Sequelize('Url_shortener', 'postgres', 'Deep@k2110', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+// });
+
+const connectionString = 'postgres://url_shortener_2nqg_user:zAkVxXSbUgG0vnoagmOAV5Utuukrs9pB@dpg-cmgjat821fec739s5odg-a.singapore-postgres.render.com/url_shortener_2nqg';
+
+const sequelize = new Sequelize(connectionString, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // To disable SSL certificate validation (use in development, not recommended for production)
+    },
+  },
+  logging: false, // Set to true if you want to see SQL logs
 });
 
 const URL = sequelize.define('URL', {
