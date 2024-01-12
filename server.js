2189f324d7bdff9c29feac8de52465539ce24d37
Deepak Kumar <dk135781@gmail.com>
2024-01-12 16:53:30 +0530

    updated

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-require('./config/mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
